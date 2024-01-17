812a00e7085d9ace06660fc86576daf37189828b
wscnd <72019998+wscnd@users.noreply.github.com>
2024-01-17 20:58:54 -0300

    dom events showcase

---
+++ app.js
@@ -0,0 +1,15 @@
+window.addEventListener("DOMContentLoaded", function (event) {
+  let nav = document.querySelector("nav")
+  console.log(nav)
+  nav.innerHTML = `
+<h2>Hello DOM</h2>
+<p>This is HTML within a Javascript string</p>
+  `
+})
+
+window.addEventListener("DOMContentLoaded", function (event) {
+})
+
+window.addEventListener("DOMContentLoaded", function (event) {
+  console.log("hiiiiiiiiii")
+})

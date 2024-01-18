737dcc4cf8e93f256d63e48d2d78cd9068fa4a50
wscnd <72019998+wscnd@users.noreply.github.com>
2024-01-18 17:22:32 -0300

    basic router

--- services/Router.js
+++ services/Router.js
@@ -1,16 +1,48 @@
 const Router = {
   init: function () {
     document.querySelectorAll("a.navlink").forEach(a => {
-      addEventListener("click", event => {
+
+      a.addEventListener("click", event => {
         event.preventDefault()
-        console.log("Link clicked");
+        this.go(event.target.getAttribute("href"))
       })
+
+      // deep linking
     })
+    Router.go(location.pathname)
   },
 
-  go: function (path, addToHistory = true) {
-    console.log(`Going to ${route}`);
-  }
+  go: (path, addToHistory = true) => {
+    console.log(`Going to ${path}`)
+
+    if (addToHistory) {
+      history.pushState({ path }, null, path)
+    }
+
+    let pageElement = null
+    switch (path) {
+      case "/":
+        pageElement = document.createElement("h1")
+        pageElement.textContent = "Menu"
+        break
+
+      case "/order":
+        pageElement = document.createElement("h1")
+        pageElement.textContent = "Your Order"
+        break
+      default:
+        break
+    }
+
+    if (pageElement) {
+      const cache = document.querySelector("main")
+      cache.innerHTML = ""
+      cache.appendChild(pageElement)
+
+      window.scrollX = 0
+      window.scrollY = 0
+    }
+  },
 }
 
 export default Router

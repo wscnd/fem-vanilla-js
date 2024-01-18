0d56a99bfbd0d7a3a73c5d3274e28114a948971a
wscnd <72019998+wscnd@users.noreply.github.com>
2024-01-18 15:34:09 -0300

    add router

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 import Store from "./services/Store.js"
 import { loadData } from "./services/Menu.js"
+import Router from "./services/Router.js"
 
 window.app = {}
 app.store = Store
+app.router = Router
 
 window.addEventListener("DOMContentLoaded", function (event) {
   loadData()
---
+++ services/Router.js
@@ -0,0 +1,10 @@
+const Router = {
+  init: function () {
+  },
+
+  go: function (path, addToHistory = true) {
+    console.log(`Going to ${route}`);
+  }
+}
+
+export default Router

209a3aac2811400d42145047d893c7969dd4efba
wscnd <72019998+wscnd@users.noreply.github.com>
2024-01-19 10:36:06 -0300

    creating custom components

--- app.js
+++ app.js
@@ -2,6 +2,18 @@ import Store from "./services/Store.js"
 import { loadData } from "./services/Menu.js"
 import Router from "./services/Router.js"
 
+/**
+ * Components
+ * Need to link for the browser understand what is happening
+ */
+import { MenuPage } from "./components/MenuPage.js"
+import { OrderPage } from "./components/OrderPage.js"
+import { DetailsPage } from "./components/DetailsPage.js"
+
+
+/**
+ * Global state
+ */
 window.app = {}
 app.store = Store
 app.router = Router
---
+++ components/MenuPage.js
@@ -0,0 +1,7 @@
+export class MenuPage extends HTMLElement {
+  constructor() {
+    super()
+  }
+}
+
+customElements.define("menu-page", MenuPage)

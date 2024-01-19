efa212eee9992257d2a091c1d83ade3735bfb0f9
wscnd <72019998+wscnd@users.noreply.github.com>
2024-01-19 17:59:32 -0300

    add getProductById service

--- services/Menu.js
+++ services/Menu.js
@@ -3,3 +3,22 @@ import API from "./API.js"
 export async function loadData() {
   app.store.menu = await API.fetchMenu()
 }
+
+export async function getProductById(pId) {
+  if (app.store.menu === null) {
+    await loadData()
+  }
+
+  console.log("Looking for product with id", pId)
+  for (const c of app.store.menu) {
+    const found = c.products.find(({ id }) => {
+      return id == pId
+    })
+
+    if (found) {
+      return found
+    }
+  }
+
+  return null
+}

3fe9653b947592400db8ccbe50af62f68c86339e
wscnd <72019998+wscnd@users.noreply.github.com>
2024-01-19 10:56:18 -0300

    solution: create a shadow dom

--- components/MenuPage.js
+++ components/MenuPage.js
@@ -1,6 +1,8 @@
 export class MenuPage extends HTMLElement {
   constructor() {
     super()
+
+    this.root = this.attachShadow({ mode: "open" })
   }
 
   // NOTE: when the component is attached to the DOM
@@ -10,7 +12,7 @@ export class MenuPage extends HTMLElement {
 
     // NOTE: the template is unusable, you have to clone and create a real instance of the template
     const content = template.content.cloneNode(true)
-    this.appendChild(content)
+    this.root.appendChild(content)
   }
 }
 

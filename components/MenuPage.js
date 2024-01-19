9e9e14bb5915d90b2e6092b753b1c4902f30c3be
wscnd <72019998+wscnd@users.noreply.github.com>
2024-01-19 10:53:00 -0300

    attaching the menu <template> to the dom

--- components/MenuPage.js
+++ components/MenuPage.js
@@ -2,6 +2,16 @@ export class MenuPage extends HTMLElement {
   constructor() {
     super()
   }
+
+  // NOTE: when the component is attached to the DOM
+  connectedCallback() {
+    /** @type {HTMLTemplateElement} */
+    const template = document.getElementById('menu-page-template');
+
+    // NOTE: the template is unusable, you have to clone and create a real instance of the template
+    const content = template.content.cloneNode(true)
+    this.appendChild(content)
+  }
 }
 
 customElements.define("menu-page", MenuPage)

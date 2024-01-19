8ad168dbae4280a594edf0cb0dec92296f5b06d3
wscnd <72019998+wscnd@users.noreply.github.com>
2024-01-19 15:21:01 -0300

    proxies: create a proxy object for the store
    so you don't manipulate it directly

--- services/Store.js
+++ services/Store.js
@@ -3,4 +3,18 @@ const Store = {
   cart: []
 }
 
-export default Store
+/** @type {ProxyHandler} */
+const handler = {
+  set(target, prop, newValue, receiver) {
+    console.log('target:', target)
+    console.log('prop:', prop)
+    console.log('newValue:', newValue)
+    console.log('receiver:', receiver)
+    return Reflect.get(...arguments)
+  }
+
+}
+
+const proxiedStore = new Proxy(store, handler)
+
+export default handler
